cc31fe46638b186ec5aba2aca97c45bdcc564380
Davi1990 <momi.davide89@gmail.com>
2020-11-14 17:13:18 +0100

    new function for connectivity

--- DissNet/metrics.py
+++ DissNet/metrics.py
@@ -13,18 +13,72 @@ import matplotlib.pyplot as plt
 
 class Connectivity_metrics(object):
 
-    def __init__(self, matrices_files, labels_dic):
+    def __init__(self, matrices_files, net_label_txt, labels_dic):
 
         self.matrices_files = matrices_files
+        self.net_label_txt = net_label_txt
         self.labels_dic = labels_dic
 
-    def extract_conn(self, sbj2use, label2use):
-        self.matrix = pd.read_csv(self.matrices_files[sbj2use], sep= ' ', header=None)
-        self.matrix = np.array(self.matrix)
-        np.fill_diagonal(self.matrix,0)
-        self.subj_matrix = self.matrix[self.labels_dic[label2use]]
-        self.subj_matrix = self.subj_matrix[:,self.labels_dic[label2use]]
-        self.streamlines_sum = np.sum(np.sum(self.subj_matrix))
-        self.conn_measure = self.streamlines_sum/len(self.labels_dic[label2use])
+    def nodes_overall_conn(self):
+        self.nodes_conn = []
+        for subj in range(len(self.matrices_files)):
+            self.matrix = pd.read_csv(self.matrices_files[subj], sep= ' ', header=None)
+            self.matrix = np.array(self.matrix)
+            np.fill_diagonal(self.matrix,0)
+            for nodes in range(self.matrix.shape[0]):
+                self._node_conn =  np.sum(self.matrix[nodes])
+                self.nodes_conn.append(self._node_conn)
+        self.nodes_conn = np.array(self.nodes_conn)
+        self.nodes_conn = self.nodes_conn.reshape(len(self.matrices_files), self.matrix.shape[0])
+        return self.nodes_conn
 
-        return self.conn_measure
+
+    def node_inner_net_conn(self, sbj_number, nodes_number):
+        with open(self.net_label_txt) as f:
+            net=f.read().splitlines()
+        self.all_conn = np.zeros([sbj_number, nodes_number])
+        for subj in range(len(self.matrices_files)):
+            self.matrix = pd.read_csv(self.matrices_files[subj], sep= ' ', header=None)
+            self.matrix = np.array(self.matrix)
+            np.fill_diagonal(self.matrix,0)
+            for network in net:
+                for nodes in self.labels_dic[network]:
+                    self.sub_matrix =self.matrix[nodes]
+                    self.all_conn[subj, nodes] = np.sum(self.sub_matrix[self.labels_dic[network]])
+        return self.all_conn
+
+
+    def node_outer_net_conn(self, sbj_number, nodes_number):
+        with open(self.net_label_txt) as f:
+            net=f.read().splitlines()
+        self.all_conn = np.zeros([sbj_number, nodes_number])
+        for subj in range(len(self.matrices_files)):
+            self.matrix = pd.read_csv(self.matrices_files[subj], sep= ' ', header=None)
+            self.matrix = np.array(self.matrix)
+            np.fill_diagonal(self.matrix,0)
+            self.nodes_ranges = np.arange(len(self.labels_dic['nodes']))
+            for network in net:
+                self.outer_idx = np.setdiff1d(self.nodes_ranges, self.labels_dic[network])
+                for nodes in self.outer_idx:
+                    self.sub_matrix =self.matrix[nodes]
+                    self.all_conn[subj, nodes] = np.sum(self.sub_matrix[self.outer_idx])
+        return self.all_conn
+
+
+    def net_inner_conn(self):
+        with open(self.net_label_txt) as f:
+            net=f.read().splitlines()
+        self.all_conn = []
+        for subj in range(len(self.matrices_files)):
+            self.matrix = pd.read_csv(self.matrices_files[subj], sep= ' ', header=None)
+            self.matrix = np.array(self.matrix)
+            np.fill_diagonal(self.matrix,0)
+            for network in net:
+                self.subj_matrix = self.matrix[self.labels_dic[network]]
+                self.subj_matrix = self.subj_matrix[:,self.labels_dic[network]]
+                self.streamlines_sum = np.sum(np.sum(self.subj_matrix))
+                self.conn_measure = self.streamlines_sum/len(self.labels_dic[network])
+                self.all_conn.append(self.conn_measure)
+        self.all_conn = np.array(self.all_conn)
+        self.all_conn = self.all_conn.reshape(len(self.matrices_files), len(net))
+        return self.all_conn
--- DissNet/plot.py
+++ DissNet/plot.py
@@ -8,40 +8,67 @@ import numpy as np
 import pandas as pd
 
 
-#os.environ['SUBJECTS_DIR'] = sys.argv[0]
 
-#subjects_dir = sys.argv[0]
-#subject = sys.argv[1]
-
-
-#np.set_printoptions(suppress=True) #prevent numpy exponential
-#label = pd.read_excel('/home/davide/Desktop/DAVE/ATLASES/Schaefer/Schafer_for_BrainNet.xlsx', header=None)
-#matrix = np.array(label)
-#coords = matrix[:,0:3]
-
-#brain = Brain(subject, subjects_dir=subjects_dir, hemi='rh', surf='pial', cortex='ivory', alpha=0.5)
-
-
-#for hemi in ["lh", "rh"]:
-#    brain = Brain(subject, subjects_dir=subjects_dir, hemi=hemi, surf='pial', cortex='ivory', alpha=0.5)
-#    brain.add_foci(coords, color="green", scale_factor=0.8)
-
-#brain.add_foci(coords, color="green", scale_factor=0.8)
-
-#mlab.show()
-def plot_atlas(subjects_dir, atlas, subject, hemi):
+def plot_surface(subjects_dir, atlas, subject, hemi, opacity=None ,scale_factor=None):
     np.set_printoptions(suppress=True) #prevent numpy exponential
     label = pd.read_excel(atlas, header=None)
     matrix = np.array(label)
-    coords = matrix[:,0:3]
+    coords = np.array(matrix[:,5:8], dtype='float32')
+    color = np.array(matrix[:,2:5], dtype='float32')
+    def NormalizeData(data):
+         return (data - np.min(data)) / (np.max(data) - np.min(data))
+    if scale_factor==None:
+     scale_factor_norm= np.zeros(matrix.shape[0]) + 0.5
+    else:
+        scale_factor_norm = NormalizeData(scale_factor) + 0.5
 
-    brain = Brain(subject, subjects_dir=subjects_dir, hemi=hemi, surf='pial', cortex='ivory', alpha=0.5)
+    if opacity==None:
+     opacity= 0.5
+    else:
+        opacity = opacity
 
-
+    brain = Brain(subject, subjects_dir=subjects_dir, hemi=hemi, surf='pial', cortex='ivory', alpha=opacity)
     #for hemi in ["lh", "rh"]:
     #    brain = Brain(subject, subjects_dir=subjects_dir, hemi=hemi, surf='pial', cortex='ivory', alpha=0.5)
     #    brain.add_foci(coords, color="green", scale_factor=0.8)
+    for roi in range(coords.shape[0]):
+        brain.add_foci(coords[roi], color=(color[roi][0]/255.0, color[roi][1]/255.0, color[roi][2]/255.0), scale_factor=scale_factor_norm[roi], hemi='rh')
 
-    brain.add_foci(coords, color="green", scale_factor=0.8)
+    #brain.add_foci(coords, color="green", scale_factor=0.8)
 
     mlab.show()
+
+
+def spider_plot(labels, values, colour, linestyle, linecolour, linewidth, label_colour, label_size, ylim):
+    values = values * 100 / np.sum(values)
+    values = values.tolist()
+    values += values[:1]
+    N = len(labels)
+    angles = [n / float(N) * 2 * pi for n in range(N)]
+    angles += angles[:1]
+    ax = plt.subplot(111, polar=True)
+
+    plt.xticks(angles, Net, color=label_colour, size=label_size)
+
+    ax.set_rlabel_position(0)
+    plt.ylim(0,ylim)
+
+    ax.plot(angles, values, linecolour , linewidth=linewidth, linestyle=linestyle)
+
+    ax.fill(angles, values, colour, alpha=0.3)
+    plt.show()
+
+
+
+def bar_plot(Net_label, values, align, alpha, xlabel):
+    barlist= plt.barh(np.arange(len(Net_label)), values, align=align, alpha=alpha)
+    plt.yticks(np.arange(len(Net_label)), Net_label)
+    plt.xlabel(xlabel)
+    barlist[0].set_color('purple')
+    barlist[1].set_color('skyblue')
+    barlist[2].set_color('green')
+    barlist[3].set_color('violet')
+    barlist[4].set_color('wheat')
+    barlist[5].set_color('orange')
+    barlist[6].set_color('#cb181d')
+    plt.show()
